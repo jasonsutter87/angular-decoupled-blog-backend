da0542c158dd561d36d262b38372e0d19739ffac
Jason Sutter <jasonsutter@Rocko.wavecable.com>
2018-03-14 09:20:04 -0700

    Updated Contoller:

--- decoupled_blog_backend/app/controllers/posts_controller.rb
+++ decoupled_blog_backend/app/controllers/posts_controller.rb
@@ -1,10 +1,15 @@
 class PostsController < ApplicationController
 
+  #getRequest http://localhost:3000/posts
+
+
   def index
     @posts = Post.all
     render :json => @posts
   end
 
+
+  #getRequest http://localhost:3000/posts/:id
   def show
     @post = Post.find(params[:id])
   	render :json => @post
@@ -12,6 +17,13 @@ class PostsController < ApplicationController
 
   def new
   end
+
+  #postRequest http://localhost:3000/posts/
+    #Header: Content-Type (key), Content-Type (value)
+    #Body:
+      #title (key), 'Pizza' (value)
+      #author (key), 'Jason Sutter' (value)
+      #content (key), 'I like Pizza' (value)
 
   def create
     @post = Post.create(title: params[:title], author: params[:author], content: params[:content])
@@ -27,7 +39,8 @@ class PostsController < ApplicationController
   	render :json => @post
   end
 
-  def destory
+  #deleteRequest http://localhost:3000/posts/:id
+  def destroy
     @post = Post.find(params[:id])
   	@post.destroy()
   	redirect_to posts_path
